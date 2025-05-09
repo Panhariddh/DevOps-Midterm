ea0fd85c3298a286a4dcb24736c55d25055b31c2
Chea Oudompanhariddh <cheaodompanhariddh.com>
2025-05-09 11:21:30 +0700

    first commit

---
+++ src/main/java/i4/gic/library_webapp_chea_oudompanhariddh/LibraryWebappCheaOudompanhariddhApplication.java
@@ -0,0 +1,13 @@
+package i4.gic.library_webapp_chea_oudompanhariddh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebappCheaOudompanhariddhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebappCheaOudompanhariddhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/i4/gic/library_webapp_chea_oudompanhariddh/LibraryWebappCheaOudompanhariddhApplicationTests.java
@@ -0,0 +1,13 @@
+// package i4.gic.library_webapp_chea_oudompanhariddh;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class LibraryWebappCheaOudompanhariddhApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
